7619a34c675d20f45670388bdee78fab84423070
Camilo Carmona Valencia <101128930+Camilin1904@users.noreply.github.com>
2023-12-12 21:43:56 -0500

    Reformat minor, what the fuck where you doing in home?

--- Task/views/home.py
+++ Task/views/home.py
@@ -10,14 +10,10 @@ from django.contrib.auth.decorators import user_passes_test, login_required
 def home(request):   
     
     user = request.user
-    tasks = Task.objects.all()
-    user_tasks = []
-    for task in tasks:
-        if(task.user == user):
-            user_tasks.append(task)
+    tasks = Task.objects.filter(user = request.user)
 
     return render(request, './home.html', {
         'user': user,
-        'tasks': reversed(user_tasks)
+        'tasks': tasks
     })
     
